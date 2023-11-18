63bc1de8a2f9b5843ec9d008916b86460c6a0ffd
littlerobot54 <llogarme26@gmail.com>
2023-11-18 01:16:42 +0100

    Icons and colors removed from scripts

--- .config/bin/target_ip.sh
+++ .config/bin/target_ip.sh
@@ -4,7 +4,7 @@ ip_address=$(cat ~/.config/bin/target | awk '{print $1}')
 machine_name=$(cat ~/.config/bin/target | awk '{print $2}')
  
 if [ $ip_address ] && [ $machine_name ]; then
-    echo "%{F#e51d0b}什 %{F#ffffff}$ip_address%{u-} - $machine_name"
+    echo "$ip_address - $machine_name"
 else
-    echo "%{F#e51d0b}什 %{u-}%{F#ffffff} No target"
+    echo "No target"
 fi
